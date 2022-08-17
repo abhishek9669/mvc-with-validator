e39c81b0f5091993960b36d5bdb78c8a146a6457
abhishek9669 <abhisheksuthar9669@gmail.com>
2022-08-17 11:55:01 +0530

    a

--- src/controller/admin/adminController.js
+++ src/controller/admin/adminController.js
@@ -4,8 +4,17 @@ const { Cat } = require('../../models/cat');
 var adminController = (req,res)=>{
 
     const kitty = new Cat({ name: req.query.name });
-    kitty.save().then(() => console.log('done'));
-    res.status(200).json({message:"hello"});
+    kitty.save().then(() => {
+        res.status(200).json({
+            'msg':"done"
+        });
+    }).catch(e=>{
+        res.status(400).json({
+            'msg':"error",
+           
+        });
+    });
+
     
 }
 
